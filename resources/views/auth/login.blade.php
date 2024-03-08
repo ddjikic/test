33d0f9bbf77165170cffd928c511017d8eb3b0e0
damir djikic <ddjikic@gmail.com>
2024-03-08 03:50:21 +0100

    add register a new account button

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -42,6 +42,11 @@
             <x-primary-button class="ms-3">
                 {{ __('Log in') }}
             </x-primary-button>
+
         </div>
+        <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{
+                route('register') }}">
+            {{ __('Register a new account') }}
+        </a>
     </form>
 </x-guest-layout>
